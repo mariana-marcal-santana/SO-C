deb26c1b7c6029c51242ef5ab084b1e777fa1f9a
Mariana Santana <mariana.marcal.santana@tecnico.ulisboa.pt>
2023-12-03 01:23:01 +0000

    commit

--- p1_base/files.c
+++ p1_base/files.c
@@ -8,14 +8,20 @@
 
 void exeDir(DIR *dir) {
     struct dirent *entry;
+    char currentPath[256];
 
     while ((entry = readdir(dir)) != NULL) {
+        getcwd(currentPath, sizeof(currentPath));
         // Verifica se o nome do arquivo tem a extensão .jobs
         if (strcmp(entry->d_name + strlen(entry->d_name) - 5, ".jobs") == 0) {
             // Abre o arquivo de entrada
-            int fd_input = open(entry->d_name, O_RDONLY);
+            strcat(currentPath,"/jobs/");
+            strcat(currentPath, entry->d_name);
+            printf("%s\n", entry->d_name);
+
+            int fd_input = open(currentPath, O_RDONLY);
             if (fd_input == -1) {
-                perror("Failed to open input file");
+                perror("Failed to open input file1");
                 fprintf(stderr, "File name: %s\n", entry->d_name);
                 continue;
             }
--- p1_base/main.c
+++ p1_base/main.c
@@ -13,19 +13,8 @@ int main(int argc, char *argv[]) {
   unsigned int state_access_delay_ms = STATE_ACCESS_DELAY_MS;
 
   if (argc > 1) {
-    char *endptr;
-    unsigned long int delay = strtoul(argv[1], &endptr, 10);
-
-    if (*endptr != '\0' || delay > UINT_MAX) {
-      fprintf(stderr, "Invalid delay value or value too large\n");
-      return 1;
-    }
-    state_access_delay_ms = (unsigned int)delay;
-
-  }
-  if (argc > 2) {
-    char * path = argv[2];
-    DIR * dir = opendir(path);
+    char * path = argv[1];
+    DIR *dir = opendir(path);
     
     if (dir == NULL) {
       fprintf(stderr, "Invalid path\n");
@@ -36,6 +25,19 @@ int main(int argc, char *argv[]) {
       closedir(dir);
     }
   }
+
+  if (argc > 2) {
+    char *endptr;
+    unsigned long int delay = strtoul(argv[2], &endptr, 10);
+
+    if (*endptr != '\0' || delay > UINT_MAX) {
+      fprintf(stderr, "Invalid delay value or value too large\n");
+      return 1;
+    }
+    state_access_delay_ms = (unsigned int)delay;
+
+  }
+
   if (ems_init(state_access_delay_ms)) {
     fprintf(stderr, "Failed to initialize EMS\n");
     return 1;
