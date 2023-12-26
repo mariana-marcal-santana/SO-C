c73e454a957230dcb35f3e5ab614e530f6ea45c3
jlucas <117687632+l28j@users.noreply.github.com>
2023-12-26 18:01:25 +0000

    commit

--- Proj2/proj_23-24-p2_base/client/api.c
+++ Proj2/proj_23-24-p2_base/client/api.c
@@ -8,7 +8,7 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
-int id_session = -1 ;
+int id_session ;
 
 int ems_setup(char const* req_pipe_path, char const* resp_pipe_path, char const* server_pipe_path) {
   //TODO: create pipes and connect to the server
@@ -21,6 +21,14 @@ int ems_setup(char const* req_pipe_path, char const* resp_pipe_path, char const*
   char *path_response = malloc(len_response);
   snprintf(path_response, len_response, "%s", resp_pipe_path);
  
+
+  //Connect to the server pipe
+  int fd_server_resquest = open(server_pipe_path, O_WRONLY);
+  if (fd_server_resquest == -1) {
+    fprintf(stderr, "Error opening server pipe\n");
+    return 1;
+  }
+
   //Create the pipes
   if(mkfifo(path_request, 0777) == -1){
     fprintf(stderr, "Error creating request pipe\n");
@@ -32,46 +40,50 @@ int ems_setup(char const* req_pipe_path, char const* resp_pipe_path, char const*
     return 1;
   }
 
-  //Connect to the server pipe
-  int fd_server = open(server_pipe_path, O_WRONLY);
-  if (fd_server == -1) {
-    fprintf(stderr, "Error opening server pipe\n");
-    return 1;
-  }
-
   //Set the message to send to the server
-  char buffer[82] ;
+  char buffer[84] ;
 
   buffer[0] = '1';
   buffer[1] = '\0';
 
   if (len_request < 40){
     strcat(buffer, path_request);
-    memset(buffer + 2 , '\0', 40 ); 
+    memset(buffer + 2 , '\0', 43); 
+  }
+  else{
+    strcat(buffer, path_request);
   }
   if (len_response < 40){
     strcat(buffer, path_response);
-    memset(buffer + 42 , '\0', 40 ); 
+    memset(buffer + 44 , '\0', 84);
   }
-
-  strcat(buffer, path_request);
-  strcat(buffer, path_response);
-
+  else{
+    strcat(buffer, path_response);
+  }
+ 
   //Send the message to the server
 
-  if (write(fd_server, buffer, 82) == -1) {
+  if (write(fd_server_resquest, buffer, 82) == -1) {
     fprintf(stderr, "Error writing to server pipe\n");
     return 1;
   }
 
   //Close the server pipe
-  if (close(fd_server) == -1) {
+  if (close(fd_server_resquest) == -1) {
     fprintf(stderr, "Error closing server pipe\n");
     return 1;
   }
 
   free(path_request);
   free(path_response);
+
+  //Read the response from the server
+  /*int fd_server_response = open(req_pipe_path, O_RDONLY);
+  if (fd_server_response == -1) {
+    fprintf(stderr, "Error opening server pipe\n");
+    return 1;
+  }*/
+
   return 0;
 }
 
--- Proj2/proj_23-24-p2_base/server/main.c
+++ Proj2/proj_23-24-p2_base/server/main.c
@@ -10,58 +10,131 @@
 #include <errno.h>
 #include <pthread.h>
 #include <semaphore.h>
-
-
-
-
-
-
+#include <string.h> // Add this line for the strcmp function
 
 #include "common/constants.h"
 #include "common/io.h"
 #include "operations.h"
 
+char *path_register_FIFO ; 
+
+struct Client{
+  int id_session ;
+  char *path_request ;
+  char *path_response ;
+  pthread_t thread ;
+};
+
+struct Client clients[MAX_SESSION_COUNT] ;
 
 
+void set_list_Clients(struct Client *clients){
+  for (int i = 0; i < MAX_SESSION_COUNT; i++){
+    clients[i].id_session = -1 ;
+    clients[i].path_request = NULL ;
+    clients[i].path_response = NULL ;
+  }
+}
+
+void set_Client(struct Client *clients, int id_session, char *path_request, char *path_response){
+  for (int i = 0; i < MAX_SESSION_COUNT; i++){
+    if (clients[i].id_session == -1){
+      clients[i].id_session = id_session ;
+      clients[i].path_request = path_request ;
+      clients[i].path_response = path_response ;
+      break ;
+    }
+  }
+}
+
+void remove_CLient(struct Client *clients, int id_session){
+  for (int i = 0; i < MAX_SESSION_COUNT; i++){
+    if (clients[i].id_session == id_session){
+      clients[i].id_session = -1 ;
+      clients[i].path_request = NULL ;
+      clients[i].path_response = NULL ;
+      break ;
+    }
+  }
+}
+
+int get_free_index(struct Client *clients){
+  for (int i = 0; i < MAX_SESSION_COUNT; i++){
+    if (clients[i].id_session == -1){
+      return i ;
+    }
+  }
+  return -1 ;
+}
+
+
+
+void * wait_for_requests_operation(void *arg) {
+  //TODO
+}
 
 void *wait_for_requests_session(void *arg) {
+  
+  set_list_Clients(clients) ;
 
-  int session_id = 0;
+  sem_t semaphore_sessions ;
   char *path_register_FIFO = (char *)arg;
 
-  sem_t semaphore ;
-
-  if (sem_init(&semaphore, 0, MAX_SESSION_COUNT) == -1) {
+  if (sem_init(&semaphore_sessions, 0, MAX_SESSION_COUNT) == -1) {
     fprintf(stderr, "Failed to initialize semaphore\n");
     return NULL;
   }  
   while(1){
-    sem_wait(&semaphore); 
+    
+    sem_wait(&semaphore_sessions); 
+  
+    //Open register pipe to read request
     int fd_register = open(path_register_FIFO, O_RDONLY);
     if (fd_register == -1) {
       fprintf(stderr, "Error opening register pipe\n");
       return NULL;
     }
-
-    char buffer[82] ;
-    ssize_t bytes_read = read(fd_register, buffer, 82);
+    char buffer_request[82] ;
+    ssize_t bytes_read = read(fd_register, buffer_request, 82);
     if (bytes_read == -1) {
       fprintf(stderr, "Error reading from register pipe\n");
       return NULL;
     }
-
-    if (buffer[0] == '1') {
-      fprintf(stderr, "Received request: %s\n", buffer);
-    }
-
+    //Close register pipe
     if (close(fd_register) == -1) {
       fprintf(stderr, "Error closing register pipe\n");
       return NULL;
     }
-    break;
+    //Parse request
+    char request_type[2]; // Change the size to 2
+    strncpy(request_type, buffer_request, 1); // Copy the first character from buffer to request_type
+    request_type[1] = '\0'; // Add null terminator to request_type
+
+    //Verify request type
+    if (request_type[0] == '1') {
+      fprintf (stderr, "Request session\n");
+      char buffer_response[2];
+      int free_id_session = get_free_index(clients) ;
+      clients[free_id_session].id_session = free_id_session ;
+      char *path_client_request;
+      char *path_client_response;
+      path_client_request = strtok(buffer_request + 1, "\0");
+      path_client_response = strtok(buffer_request + 41, "\0");
+
+
+      
+
+
+
+
+    }
+    else if (request_type[0] == '0'){
+      fprintf (stderr, "Request quit client\n");
+      //TODO
+    }
+    
   }
 }
-
 
 int main(int argc, char* argv[]) {
   if (argc < 2 || argc > 3) {
@@ -104,14 +177,15 @@ int main(int argc, char* argv[]) {
   }
 
   //TODO: Intialize server, create worker threads
-
-  while (1) {
-    //TODO: Read from pipe
-    //TODO: Write new client to the producer-consumer buffer
+  //TODO: Read from pipe
+  //TODO: Write new client to the producer-consumer buffer
+  
+  if (pthread_join(requests_SESSIONS, NULL)) {
+    fprintf(stderr, "Failed to join thread\n");
+    return 1;
   }
 
   //TODO: Close Server
-
   ems_terminate();
   free(arg);
   free(path_register_FIFO);
